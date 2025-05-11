d4cc8f0776d964a64ef88c732829d971e597391b
Lucas LaBuff <lucasbeef00@gmail.com>
2025-05-11 12:24:10 -0400

    add version check

--- ShadowsReanimated/Plugin.cs
+++ ShadowsReanimated/Plugin.cs
@@ -1,6 +1,8 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using Shared;
+using System.Linq;
 
 namespace ShadowsReanimated;
 
@@ -10,12 +12,19 @@ public class Plugin : BaseUnityPlugin {
     const string GUID = "com.lalabuff.necrodancer.shadowsreanimated";
     const string NAME = "ShadowsReanimated";
     const string VERSION = "0.0.1";
+    readonly static string[] BUILDS = ["1.4.0-b20638"];
 
     internal static ManualLogSource Log { get; private set; }
 
     internal void Awake() {
         Log = Logger;
-        
+
+        var build = BuildInfoHelper.Instance.BuildId;
+        if(!BUILDS.Contains(build)) {
+            Log.LogFatal($"The current version of the game is not compatible with this plugin. Please update the game or the mod to the correct version. The current mod version is v{VERSION} and the current game version is {build}. Allowed game versions: {string.Join(", ", BUILDS)}");
+            return;
+        }
+
         ShadowsReanimated.Config.Initialize(Config);
         Assets.Initialize();
 
