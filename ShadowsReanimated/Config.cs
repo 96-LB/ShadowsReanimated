497ab2bf90b321a1318480c94f29542120f42c04
Cap <lucas.labuff@gmail.com>
2026-02-10 15:50:54 +0100

    rewrite for necromanager

--- ShadowsReanimated/Config.cs
+++ ShadowsReanimated/Config.cs
@@ -1,58 +1,31 @@
-﻿using BepInEx.Configuration;
+﻿using RiftOfTheNecroManager;
 using UnityEngine;
 
 namespace ShadowsReanimated;
 
 
 public static class Config {
-    public class ConfigGroup(ConfigFile config, string group) {
-        public ConfigEntry<T> Bind<T>(string key, T defaultValue, string description) {
-            return config.Bind(group, key, defaultValue, description);
-        }
-
-        public ConfigEntry<T> Bind<T>(string key, T defaultValue, ConfigDescription description = null) {
-            return config.Bind(group, key, defaultValue, description);
-        }
-    }
-
     public static class General {
-        public static ConfigEntry<PresetType> Preset;
-        public static ConfigEntry<bool> Colors;
-        public static ConfigEntry<bool> VibeChainOverride;
-        public static ConfigEntry<bool> VibePowerOverride;
-
-        public static void Initialize(ConfigGroup config) {
-            Preset = config.Bind("Preset", PresetType.Default, "Select a custom shadow preset.");
-            Colors = config.Bind("Custom Colors", true, "Enable custom colors for shadows.");
-            VibeChainOverride = config.Bind("Vibe Chain Override", false, "Override the shadow color of enemies in a vibe chain.");
-            VibePowerOverride = config.Bind("Vibe Power Override", false, "Override the shadow color of enemies when vibe power is active.");
-        }
+        public const string GROUP = "General";
+        public static Setting<PresetType> Preset { get; }  = new(GROUP, "Preset", PresetType.Default, "Select a custom shadow preset.");
+        public static Setting<bool> Colors { get; } = new(GROUP, "Custom Colors", true, "Enable custom colors for shadows.");
+        public static Setting<bool> VibeChainOverride { get; } = new(GROUP, "Vibe Chain Override", false, "Override the shadow color of enemies in a vibe chain.");
+        public static Setting<bool> VibePowerOverride { get; } = new(GROUP, "Vibe Power Override", false, "Override the shadow color of enemies when vibe power is active.");
     }
-
+    
     public static class Sprites {
-        public static ConfigEntry<SpriteType> OnBeat;
-        public static ConfigEntry<SpriteType> SixthBeat;
-        public static ConfigEntry<SpriteType> QuarterBeat;
-        public static ConfigEntry<SpriteType> ThirdBeat;
-        public static ConfigEntry<SpriteType> HalfBeat;
-        public static ConfigEntry<SpriteType> TwoThirdBeat;
-        public static ConfigEntry<SpriteType> ThreeQuarterBeat;
-        public static ConfigEntry<SpriteType> FiveSixthBeat;
-        public static ConfigEntry<SpriteType> OtherBeat;
-
-        public static void Initialize(ConfigGroup config) {
-            OnBeat = config.Bind("On Beat", SpriteType.Circle, "Sprite for the shadows of monsters aligned to the beat.");
-            SixthBeat = config.Bind("Sixth Beat", SpriteType.Star, "Sprite for the shadows of monsters aligned to the sixth-beat.");
-            QuarterBeat = config.Bind("Quarter Beat", SpriteType.LeftTriangle, "Sprite for the shadows of monsters aligned to the quarter-beat.");
-            ThirdBeat = config.Bind("Third Beat", SpriteType.LeftTrapezoid, "Sprite for the shadows of monsters aligned to the third-beat.");
-            HalfBeat = config.Bind("Half Beat", SpriteType.Diamond, "Sprite for the shadows of monsters aligned to the half-beat.");
-            TwoThirdBeat = config.Bind("Two-Third Beat", SpriteType.RightTrapezoid, "Sprite for the shadows of monsters aligned to the two-third-beat.");
-            ThreeQuarterBeat = config.Bind("Three-Quarter Beat", SpriteType.RightTriangle, "Sprite for the shadows of monsters aligned to the three-quarter-beat.");
-            FiveSixthBeat = config.Bind("Five-Sixth Beat", SpriteType.Star, "Sprite for the shadows of monsters aligned to the five-sixth-beat.");
-            OtherBeat = config.Bind("Other", SpriteType.Star, "Sprite for the shadow of all other monsters.");
-        }
+        public const string GROUP = "Custom Sprites";
+        public static Setting<SpriteType> OnBeat { get; } = new(GROUP, "On Beat", SpriteType.Circle, "Sprite for the shadows of monsters aligned to the beat.");
+        public static Setting<SpriteType> SixthBeat { get; } = new(GROUP, "Sixth Beat", SpriteType.Star, "Sprite for the shadows of monsters aligned to the sixth-beat.");
+        public static Setting<SpriteType> QuarterBeat { get; } = new(GROUP, "Quarter Beat", SpriteType.LeftTriangle, "Sprite for the shadows of monsters aligned to the quarter-beat.");
+        public static Setting<SpriteType> ThirdBeat { get; } = new(GROUP, "Third Beat", SpriteType.LeftTrapezoid, "Sprite for the shadows of monsters aligned to the third-beat.");
+        public static Setting<SpriteType> HalfBeat { get; } = new(GROUP, "Half Beat", SpriteType.Diamond, "Sprite for the shadows of monsters aligned to the half-beat.");
+        public static Setting<SpriteType> TwoThirdBeat { get; } = new(GROUP, "Two-Third Beat", SpriteType.RightTrapezoid, "Sprite for the shadows of monsters aligned to the two-third-beat.");
+        public static Setting<SpriteType> ThreeQuarterBeat { get; } = new(GROUP, "Three-Quarter Beat", SpriteType.RightTriangle, "Sprite for the shadows of monsters aligned to the three-quarter-beat.");
+        public static Setting<SpriteType> FiveSixthBeat { get; } = new(GROUP, "Five-Sixth Beat", SpriteType.Star, "Sprite for the shadows of monsters aligned to the five-sixth-beat.");
+        public static Setting<SpriteType> OtherBeat { get; } = new(GROUP, "Other", SpriteType.Star, "Sprite for the shadow of all other monsters.");
         
-        public static SpriteType GetSpriteType(BeatType beatType) => (beatType switch {
+        public static SpriteType GetSpriteType(BeatType beatType) => beatType switch {
             BeatType.OnBeat => OnBeat,
             BeatType.SixthBeat => SixthBeat,
             BeatType.QuarterBeat => QuarterBeat,
@@ -62,38 +35,27 @@ public static class Config {
             BeatType.ThreeQuarterBeat => ThreeQuarterBeat,
             BeatType.FiveSixthBeat => FiveSixthBeat,
             _ => OtherBeat
-        }).Value;
+        };
     }
-
+    
     public static class Colors {
-        public static ConfigEntry<Color> OnBeat;
-        public static ConfigEntry<Color> SixthBeat;
-        public static ConfigEntry<Color> QuarterBeat;
-        public static ConfigEntry<Color> ThirdBeat;
-        public static ConfigEntry<Color> HalfBeat;
-        public static ConfigEntry<Color> TwoThirdBeat;
-        public static ConfigEntry<Color> ThreeQuarterBeat;
-        public static ConfigEntry<Color> FiveSixthBeat;
-        public static ConfigEntry<Color> OtherBeat;
-
-        public static void Initialize(ConfigGroup config) {
-            var purple = new Color(0.6f, 0.5f, 1f);
-            var red = new Color(1f, 0.4f, 0.5f);
-            var yellow = new Color(1f, 0.9f, 0.6f);
-            var green = new Color(0.5f, 1f, 0.6f);
-            var blue = new Color(0.3f, 0.7f, 1f);
-            OnBeat = config.Bind("On Beat", purple, "Color for the shadows of monsters aligned to the beat.");
-            SixthBeat = config.Bind("Sixth Beat", red, "Color for the shadows of monsters aligned to the sixth-beat.");
-            QuarterBeat = config.Bind("Quarter Beat", yellow, "Color for the shadows of monsters aligned to the quarter-beat.");
-            ThirdBeat = config.Bind("Third Beat", green, "Color for the shadows of monsters aligned to the third-beat.");
-            HalfBeat = config.Bind("Half Beat", blue, "Color for the shadows of monsters aligned to the half-beat.");
-            TwoThirdBeat = config.Bind("Two-Third Beat", green, "Color for the shadows of monsters aligned to the two-third-beat.");
-            ThreeQuarterBeat = config.Bind("Three-Quarter Beat", yellow, "Color for the shadows of monsters aligned to the three-quarter-beat.");
-            FiveSixthBeat = config.Bind("Five-Sixth Beat", red, "Color for the shadows of monsters aligned to the five-sixth-beat.");
-            OtherBeat = config.Bind("Other", red, "Color for the shadow of all other monsters.");
-        }
-
-        public static Color GetColor(BeatType beatType) => (beatType switch {
+        public const string GROUP = "Custom Colors";
+        public static readonly Color PURPLE = new(0.6f, 0.5f, 1f);
+        public static readonly Color RED = new(1f, 0.4f, 0.5f);
+        public static readonly Color YELLOW = new(1f, 0.9f, 0.6f);
+        public static readonly Color GREEN = new(0.5f, 1f, 0.6f);
+        public static readonly Color BLUE = new(0.3f, 0.7f, 1f);
+        public static Setting<Color> OnBeat { get; } = new(GROUP, "On Beat", PURPLE, "Color for the shadows of monsters aligned to the beat.");
+        public static Setting<Color> SixthBeat { get; } = new(GROUP, "Sixth Beat", RED, "Color for the shadows of monsters aligned to the sixth-beat.");
+        public static Setting<Color> QuarterBeat { get; } = new(GROUP, "Quarter Beat", YELLOW, "Color for the shadows of monsters aligned to the quarter-beat.");
+        public static Setting<Color> ThirdBeat { get; } = new(GROUP, "Third Beat", GREEN, "Color for the shadows of monsters aligned to the third-beat.");
+        public static Setting<Color> HalfBeat { get; } = new(GROUP, "Half Beat", BLUE, "Color for the shadows of monsters aligned to the half-beat.");
+        public static Setting<Color> TwoThirdBeat { get; } = new(GROUP, "Two-Third Beat", GREEN, "Color for the shadows of monsters aligned to the two-third-beat.");
+        public static Setting<Color> ThreeQuarterBeat { get; } = new(GROUP, "Three-Quarter Beat", YELLOW, "Color for the shadows of monsters aligned to the three-quarter-beat.");
+        public static Setting<Color> FiveSixthBeat { get; } = new(GROUP, "Five-Sixth Beat", RED, "Color for the shadows of monsters aligned to the five-sixth-beat.");
+        public static Setting<Color> OtherBeat { get; }  = new(GROUP, "Other", RED, "Color for the shadow of all other monsters.");
+        
+        public static Color GetColor(BeatType beatType) => beatType switch {
             BeatType.OnBeat => OnBeat,
             BeatType.SixthBeat => SixthBeat,
             BeatType.QuarterBeat => QuarterBeat,
@@ -103,21 +65,6 @@ public static class Config {
             BeatType.ThreeQuarterBeat => ThreeQuarterBeat,
             BeatType.FiveSixthBeat => FiveSixthBeat,
             _ => OtherBeat
-        }).Value;
-    }
-
-    public static class VersionControl {
-        public static ConfigEntry<string> VersionOverride;
-
-        public static void Initialize(ConfigGroup config) {
-            VersionOverride = config.Bind("Version Override", "", "Input the current build version or '*' to override the version check.");
-        }
-    }
-
-    public static void Initialize(ConfigFile config) {
-        General.Initialize(new(config, "General"));
-        Sprites.Initialize(new(config, "Custom Sprites"));
-        Colors.Initialize(new(config, "Custom Colors"));
-        VersionControl.Initialize(new(config, "Version Control"));
+        };
     }
 }
